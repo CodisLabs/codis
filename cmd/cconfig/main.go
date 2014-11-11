36062f196afbfd45262bed0786f8713b1d95f7c7
dongxu <huang@wandoujia.com>
2014-11-11 14:47:37 +0800

    remove orphan lock

--- cmd/cconfig/main.go
+++ cmd/cconfig/main.go
@@ -4,12 +4,15 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"os"
 	"os/signal"
+	"path"
 	"syscall"
 
+	"github.com/ngaut/go-zookeeper/zk"
 	"github.com/ngaut/zkhelper"
 	"github.com/wandoulabs/codis/pkg/utils"
 
@@ -29,6 +32,7 @@ var productName string
 var configFile string
 var config *cfg.Cfg
 var zkLock zkhelper.ZLocker
+var livingNode string
 
 type Command struct {
 	Run   func(cmd *Command, args []string)
@@ -55,6 +59,7 @@ commands:
 `
 
 func Fatal(msg interface{}) {
+	unRegisterConfigNode()
 	// try unlock force
 	if zkLock != nil {
 		zkLock.Unlock()
@@ -65,6 +70,82 @@ func Fatal(msg interface{}) {
 		log.Fatal(msg)
 	case error:
 		log.Fatal(errors.ErrorStack(msg.(error)))
+	}
+}
+
+func removeOrphanLocks() error {
+	nodeDir := fmt.Sprintf("/zk/codis/db_%s/living-codis-config", productName)
+	lockDir := fmt.Sprintf("/zk/codis/db_%s/LOCK", productName)
+
+	livingCfgNodes, _, err := zkConn.Children(nodeDir)
+	if err != nil {
+		return err
+	}
+
+	// get living nodes
+	nodeMap := make(map[string]struct{})
+	for _, nodeName := range livingCfgNodes {
+		nodeMap[nodeName] = struct{}{}
+	}
+
+	// get living lock
+	livingLocks, _, err := zkConn.Children(lockDir)
+	for _, lockName := range livingLocks {
+		data, _, err := zkConn.Get(path.Join(lockDir, lockName))
+		if err != nil {
+			return err
+		}
+
+		// get lock info
+		var d map[string]interface{}
+		err = json.Unmarshal(data, &d)
+		if err != nil {
+			return err
+		}
+
+		nodeName := fmt.Sprintf("%v-%v", d["hostname"], d["pid"])
+		// remove the locks that no one owns
+		if _, ok := nodeMap[nodeName]; !ok {
+			log.Info("remove orphan lock", lockName)
+			zkConn.Delete(path.Join(lockDir, lockName), 0)
+		}
+	}
+	return nil
+}
+
+func registerConfigNode() error {
+	zkPath := fmt.Sprintf("/zk/codis/db_%s/living-codis-config", productName)
+
+	hostname, err := os.Hostname()
+	if err != nil {
+		return err
+	}
+	pid := os.Getpid()
+
+	content := fmt.Sprintf(`{"hostname": "%v", "pid": %v}`, hostname, pid)
+	nodeName := fmt.Sprintf("%v-%v", hostname, pid)
+
+	zkhelper.CreateRecursive(zkConn, zkPath, "", 0, zkhelper.DefaultDirACLs())
+
+	pathCreated, err := zkConn.Create(path.Join(zkPath, nodeName), []byte(content),
+		zk.FlagEphemeral, zkhelper.DefaultDirACLs())
+
+	log.Info("living node created:", pathCreated)
+
+	if err != nil {
+		return err
+	}
+
+	livingNode = pathCreated
+
+	return nil
+}
+
+func unRegisterConfigNode() {
+	if len(livingNode) > 0 {
+		if exists, _, _ := zkConn.Exists(livingNode); exists {
+			zkConn.Delete(livingNode, 0)
+		}
 	}
 }
 
@@ -135,6 +216,14 @@ func main() {
 
 	log.Debugf("product: %s", productName)
 	log.Debugf("zk: %s", zkAddr)
+
+	if err := removeOrphanLocks(); err != nil {
+		log.Error(err)
+	}
+	if err := registerConfigNode(); err != nil {
+		log.Error(err)
+	}
+	defer unRegisterConfigNode()
 
 	cmd := args["<command>"].(string)
 	cmdArgs := args["<args>"].([]string)
