c772a80b160d09a5f059609a23d4e2ad43810d3a
spinlock <wnzheng@gmail.com>
2015-06-01 15:19:47 +0800

    backup router

--- pkg/proxy/router2/slots.go
+++ pkg/proxy/router2/slots.go
@@ -1,9 +1,20 @@
 package router
 
-import "sync"
+import (
+	"sync"
+
+	"github.com/wandoulabs/codis/pkg/models"
+	"github.com/wandoulabs/codis/pkg/proxy/group"
+)
 
 type Slot struct {
 	Id int
 
+	Info  *models.Slot
+	Group *models.ServerGroup
+
+	dst, src struct {
+		group *group.Group
+	}
 	jobs sync.WaitGroup
 }
