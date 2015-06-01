3ff3c3736e2d386c1e00b96343cfab1b3dc36bbc
spinlock <wnzheng@gmail.com>
2015-06-01 19:07:49 +0800

    slot add rwlock

--- pkg/proxy/router/slots.go
+++ pkg/proxy/router/slots.go
@@ -16,5 +16,14 @@ type Slot struct {
 	dst, src struct {
 		group *group.Group
 	}
+	lock sync.RWMutex
 	jobs sync.WaitGroup
+}
+
+func (s *Slot) Acquire() {
+	s.lock.Lock()
+}
+
+func (s *Slot) Release() {
+	s.lock.Unlock()
 }
