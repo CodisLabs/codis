88ab15cc19d168510e90415204e22fdaa4d9a7f1
spinlock <wnzheng@gmail.com>
2015-11-26 10:30:31 +0800

    Add, sync2.Future

---
+++ pkg/utils/sync2/future.go
@@ -0,0 +1,35 @@
+package sync2
+
+import "sync"
+
+type Future struct {
+	sync.Mutex
+	wait sync.WaitGroup
+	vmap map[string]interface{}
+}
+
+func (f *Future) lazyInit() {
+	if f.vmap == nil {
+		f.vmap = make(map[string]interface{})
+	}
+}
+
+func (f *Future) Add() {
+	f.wait.Add(1)
+}
+
+func (f *Future) Done(key string, val interface{}) {
+	f.Lock()
+	defer f.Unlock()
+	f.lazyInit()
+	f.vmap[key] = val
+	f.wait.Done()
+}
+
+func (f *Future) Wait() map[string]interface{} {
+	f.wait.Wait()
+	f.Lock()
+	defer f.Unlock()
+	f.lazyInit()
+	return f.vmap
+}
