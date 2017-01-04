0a8a06c4e1e148d887a1328d9a9fb30ce092b7fd
熊思 <sxiong@hust.edu.cn>
2017-01-04 11:13:59 +0800

    add fe support in docker start script (#1107)
    * add fe support in docker start script
    
    * modify ip
    
    * modify ip
    
    * Update docker.sh
    
    * Delete codis.json
    
    * update cleanup adding rm zk node

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -10,6 +10,14 @@ fi
 mkdir -p log
 
 case "$1" in
+zookeeper)
+    docker rm -f      "Codis-Z2181" &> /dev/null
+    docker run --name "Codis-Z2181" -d \
+            --read-only \
+            -p 2181:2181 \
+            jplock/zookeeper
+    ;;
+    
 dashboard)
     docker rm -f      "Codis-D28080" &> /dev/null
     docker run --name "Codis-D28080" -d \
@@ -42,6 +50,15 @@ server)
     done
     ;;
 
+fe)
+    docker rm -f      "Codis-F8080" &> /dev/null
+    docker run --name "Codis-F8080" -d \
+         -v `realpath log`:/codis/log \
+         -p 8080:8080 \
+     codis-image \
+     codis-fe -l log/fe.log --zookeeper ${hostip}:2181 --listen=0.0.0.0:8080 --assets=/gopath/src/github.com/CodisLabs/codis/bin/assets
+    ;;
+
 cleanup)
     docker rm -f      "Codis-D28080" &> /dev/null
     docker rm -f      "Codis-P29000" &> /dev/null
@@ -49,8 +66,9 @@ cleanup)
         let port="26379 + i"
         docker rm -f      "Codis-S${port}" &> /dev/null
     done
+    docker rm -f      "Codis-Z2181" &> /dev/null
     ;;
-
+    
 *)
     echo "wrong argument(s)"
     ;;
