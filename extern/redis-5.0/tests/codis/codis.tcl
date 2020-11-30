dafb1fc7592ad123acd67cccc76fd6a95d729605
pingfan108 <pifu108@qq.com>
2020-11-30 12:24:56 +0000

    [server][test] add more test cases for codis sync migration

--- extern/redis-5.0/tests/codis/codis.tcl
+++ extern/redis-5.0/tests/codis/codis.tcl
@@ -4,26 +4,74 @@
 # This software is released under the BSD License. See the COPYING file for
 # more information.
 
-proc sync_migrate_key {src_id dst_id key {tag 0}} {
-    set dst_host [get_instance_attrib redis $dst_id host]
-    set dst_port [get_instance_attrib redis $dst_id port]
+proc get_key_slot {id key} {
+    set res [R $id slotshashkey $key]
+    return [lindex $res 0]
+}
+
+proc get_slot_size {id slot} {
+    set res [R $id slotsinfo $slot 1]
+    set slot_info [lindex $res 0]
+    set slot_size [lindex $slot_info 1]
+    if {[string compare $slot_size ""] == 0} {
+        return 0
+    }
+    return $slot_size
+}
+
+proc creat_some_keys {id prefix type {num 1} {start 0}} {
+    set max_idx [expr {$start + $num}]
+    for {set j $start} {$j < $max_idx} {incr j} {
+        set key "$prefix:$j"
+        if {[string compare $type "hash"] == 0} {
+            R $id hset $key a $j b [expr $j+1] c [expr $j+2]
+        } elseif {[string compare $type "zset"] == 0} {
+            R $id zadd $key $j a [expr $j+1] b [expr $j+2] c
+        } elseif {[string compare $type "set"] == 0} {
+            R $id sadd $key a $j b [expr $j+1] c [expr $j+2]
+        } elseif {[string compare $type "list"] == 0} {
+            R $id lpush $key a $j b [expr $j+1] c [expr $j+2]
+        } else {
+            puts "unknown type: $type"
+            assert {1 == 0}
+        }
+    }
+    return $max_idx
+}
+
+proc sync_migrate_key {src dst key {tag 1}} {
+    set dst_host [get_instance_attrib redis $dst host]
+    set dst_port [get_instance_attrib redis $dst port]
     set timeout 10;  # seconds
     if {$tag == 0} {
-        set res [R $src_id SLOTSMGRTONE $dst_host $dst_port $timeout $key]
+        set res [R $src SLOTSMGRTONE $dst_host $dst_port $timeout $key]
     } else {
-        set res [R $src_id SLOTSMGRTTAGONE $dst_host $dst_port $timeout $key]
+        set res [R $src SLOTSMGRTTAGONE $dst_host $dst_port $timeout $key]
     }
     return $res
 }
 
-proc sync_migrate_slot {src_id dst_id slot {tag 0}} {
-    set dst_host [get_instance_attrib redis $dst_id host]
-    set dst_port [get_instance_attrib redis $dst_id port]
+proc sync_migrate_slot {src dst slot {tag 1}} {
+    # init the parameters for the migration
+    set dst_host [get_instance_attrib redis $dst host]
+    set dst_port [get_instance_attrib redis $dst port]
     set timeout 10;  # seconds
     if {$tag == 0} {
-        set res [R $src_id SLOTSMGRTSLOT $dst_host $dst_port $timeout $key]
+        set cmd SLOTSMGRTSLOT
     } else {
-        set res [R $src_id SLOTSMGRTTAGSLOT$dst_host $dst_port $timeout $slot]
+        set cmd SLOTSMGRTTAGSLOT
     }
+
+    # circularly migrate the slot from $src to $dst
+    set round 0
+    set succ 0
+    while 1 {
+        incr round
+        set res [R $src $cmd $dst_host $dst_port $timeout $slot]
+        incr succ [lindex $res 0]
+        set size [lindex $res 1]
+        if {$size == 0} break
+    }
+    set res [list $round $succ]
     return $res
 }
--- extern/redis-5.0/tests/codis/tests/01-sync-migration.tcl
+++ extern/redis-5.0/tests/codis/tests/01-sync-migration.tcl
@@ -4,47 +4,167 @@ source "../tests/includes/init-tests.tcl"
 
 test "Migrate one untagged key by sync method" {
     # init the env
-    set src_id 0
-    set dst_id 1
-    R $src_id flushall
-    R $dst_id flushall
-    R $src_id debug populate 2 "test"
-    R $src_id lpush ltest a b c d
-    R $src_id hset htest h1 v1 h2 v2 h3 v3
-    R $src_id zadd ztest 0 a 1 b 2 c 3 d
-    R $src_id sadd stest a b c d
-    set dig_src [R $src_id debug digest]
+    puts "Starting..."
+    set src 0; R $src flushall;
+    set dst 1; R $dst flushall;
+    R $src debug populate 2 "test"
+    R $src lpush ltest a b c d
+    R $src hset htest h1 v1 h2 v2 h3 v3
+    R $src zadd ztest 0 a 1 b 2 c 3 d
+    R $src sadd stest a b c d
+    set dig_src [R $src debug digest]
+    assert_equal OK [R $src slotscheck]
+    puts ">>> Init the enviroment: PASS"
+
+    # select the command for migration
+    set tag 0;  # 0 means SLOTSMGRTONE, while 1 means SLOTSMGRTTAGONE
 
     # check the migration of a non-existent key
-    assert_equal 0 [sync_migrate_key $src_id $dst_id "none"]
+    assert_equal 0 [sync_migrate_key $src $dst "none" $tag]
+    puts ">>> Migrate a non-existent key: PASS"
 
     # check the migration of a string type key
-    set dig_vals [R $src_id debug digest-value "test:0" "test:1"]
-    assert_equal 1 [sync_migrate_key $src_id $dst_id "test:0"]
-    assert_equal 1 [sync_migrate_key $src_id $dst_id "test:1"]
-    assert_equal $dig_vals [R $dst_id debug digest-value "test:0" "test:1"]
+    set dig_vals [R $src debug digest-value "test:0" "test:1"]
+    assert_equal 1 [sync_migrate_key $src $dst "test:0" $tag]
+    assert_equal 1 [sync_migrate_key $src $dst "test:1" $tag]
+    assert_equal $dig_vals [R $dst debug digest-value "test:0" "test:1"]
+    assert_equal OK [R $src slotscheck]
+    assert_equal OK [R $dst slotscheck]
+    puts ">>> Migrate a string type key: PASS"
 
     # check the migration of a list type key
-    set dig_val [R $src_id debug digest-value "ltest"]
-    assert_equal 1 [sync_migrate_key $src_id $dst_id "ltest"]
-    assert_equal $dig_val [R $dst_id debug digest-value "ltest"]
+    set dig_val [R $src debug digest-value "ltest"]
+    assert_equal 1 [sync_migrate_key $src $dst "ltest" $tag]
+    assert_equal $dig_val [R $dst debug digest-value "ltest"]
+    assert_equal OK [R $src slotscheck]
+    assert_equal OK [R $dst slotscheck]
+    puts ">>> Migrate a list type key: PASS"
 
     # check the migration of a hash type key
-    set dig_val [R $src_id debug digest-value "htest"]
-    assert_equal 1 [sync_migrate_key $src_id $dst_id "htest"]
-    assert_equal $dig_val [R $dst_id debug digest-value "htest"]
+    set dig_val [R $src debug digest-value "htest"]
+    assert_equal 1 [sync_migrate_key $src $dst "htest" $tag]
+    assert_equal $dig_val [R $dst debug digest-value "htest"]
+    assert_equal OK [R $src slotscheck]
+    assert_equal OK [R $dst slotscheck]
+    puts ">>> Migrate a hash type key: PASS"
 
     # check the migration of a zset type key
-    set dig_val [R $src_id debug digest-value "ztest"]
-    assert_equal 1 [sync_migrate_key $src_id $dst_id "ztest"]
-    assert_equal $dig_val [R $dst_id debug digest-value "ztest"]
+    set dig_val [R $src debug digest-value "ztest"]
+    assert_equal 1 [sync_migrate_key $src $dst "ztest" $tag]
+    assert_equal $dig_val [R $dst debug digest-value "ztest"]
+    assert_equal OK [R $src slotscheck]
+    assert_equal OK [R $dst slotscheck]
+    puts ">>> Migrate a zset type key: PASS"
 
     # check the migration of a set type key
-    set dig_val [R $src_id debug digest-value "stest"]
-    assert_equal 1 [sync_migrate_key $src_id $dst_id "stest"]
-    assert_equal $dig_val [R $dst_id debug digest-value "stest"]
+    set dig_val [R $src debug digest-value "stest"]
+    assert_equal 1 [sync_migrate_key $src $dst "stest" $tag]
+    assert_equal $dig_val [R $dst debug digest-value "stest"]
+    assert_equal OK [R $src slotscheck]
+    assert_equal OK [R $dst slotscheck]
+    puts ">>> Migrate a set type key: PASS"
 
     # verify the data isn't corrupted or changed after migration
-    assert_equal $dig_src [R $dst_id debug digest]
+    assert_equal $dig_src [R $dst debug digest]
+    puts -nonewline ">>> End of the case: "
 }
 
+test "Migrate one tagged key by sync method" {
+    # init the env
+    puts "Starting..."
+    set src 0; R $src flushall;
+    set dst 1; R $dst flushall;
+    set prefix "{test}"
+    set count [randomInt 10]; incr count;  # avoid the bad case: count == 0
+    R $src debug populate $count $prefix
+    set total $count
+    set total [creat_some_keys $src $prefix "hash" $count $total]
+    set total [creat_some_keys $src $prefix "zset" $count $total]
+    set total [creat_some_keys $src $prefix "set" $count $total]
+    set total [creat_some_keys $src $prefix "list" $count $total]
+    set dig_src [R $src debug digest]
+    assert_equal OK [R $src slotscheck]
+    puts ">>> Init the enviroment(count=$count,total=$total): PASS"
+
+    # select the command for migration
+    set tag 1;  # 0 means SLOTSMGRTONE, while 1 means SLOTSMGRTTAGONE
+
+    # check the migration of a non-existent tagged key
+    assert_equal 0 [sync_migrate_key $src $dst "{none}" $tag]
+    puts ">>> Migrate a non-existent key: PASS"
+
+    # check the migration of a randomly picked tagged key
+    set rand [randomInt $total]
+    set key "$prefix:$rand"
+    set type [R $src type $key]
+    set dig_val [R $src debug digest-value $key]
+    assert_equal $total [sync_migrate_key $src $dst $key $tag]
+    assert_equal $dig_val [R $dst debug digest-value $key]
+    assert_equal OK [R $src slotscheck]
+    assert_equal OK [R $dst slotscheck]
+    puts ">>> Migrate a tagged key($type, $key){#$src => #$dst}: PASS"
+
+    # check the migration of another tagged key with different type
+    set j [expr {($rand + $count) % $total}]
+    set key "$prefix:$j"
+    set type [R $dst type $key]
+    set dig_val [R $dst debug digest-value $key]
+    assert_equal $total [sync_migrate_key $dst $src $key $tag]
+    assert_equal $dig_val [R $src debug digest-value $key]
+    assert_equal OK [R $src slotscheck]
+    assert_equal OK [R $dst slotscheck]
+    puts ">>> Migrate a tagged key($type, $key){#$dst => #$src}: PASS"
+
+    # verify the data isn't corrupted or changed after migration
+    assert_equal $dig_src [R $src debug digest]
+    puts -nonewline ">>> End of the case: "
+}
+
+test "Migrate one static slot(no writing) by sync method" {
+    # init the env
+    puts "Starting..."
+    set src 0; R $src flushall;
+    set dst 1; R $dst flushall;
+    R $src debug populate 102400;  # almost 100 keys in each slot
+    set rand [randomInt 102400]
+    set prefix "{test_$rand}"
+    set slot [get_key_slot $src $prefix]
+    creat_some_keys $src $prefix "hash" 10 0
+    creat_some_keys $src $prefix "zset" 10 10
+    creat_some_keys $src $prefix "set" 10 20
+    creat_some_keys $src $prefix "list" 10 30
+    assert_equal OK [R $src slotscheck]
+    puts ">>> Init the enviroment(slot=$slot,prefix=$prefix): PASS"
+
+    # record the digest and slot size brfore migration
+    set dig_src [R $src debug digest]
+    set bak_size [get_slot_size $src $slot]
+
+    # migrate the slot from $src to $dst by SLOTSMGRTSLOT
+    set tag 0
+    set res [sync_migrate_slot $src $dst $slot $tag]
+    set round1 [lindex $res 0]
+    assert_equal $bak_size [lindex $res 1];    # succ == original slot size
+    set dst_size [get_slot_size $dst $slot]
+    assert_equal $bak_size $dst_size;          # all the keys have been moved
+    assert_equal 0 [get_slot_size $src $slot]; # nothing has been left on $src
+    assert_equal OK [R $src slotscheck]
+    assert_equal OK [R $dst slotscheck]
+    puts ">>> Migrate slot_$slot{#$src => #$dst} by SLOTSMGRTSLOT: PASS"
+
+    # migrate the slot from $dst to $src by SLOTSMGRTTAGSLOT
+    set tag 1
+    set res [sync_migrate_slot $dst $src $slot $tag]
+    assert {[lindex $res 0] < $round1};        # *TAG* cmd should be faster
+    assert_equal $bak_size [lindex $res 1];    # succ == original slot size
+    set src_size [get_slot_size $src $slot];
+    assert_equal $bak_size $src_size;          # all the keys have been moved
+    assert_equal 0 [get_slot_size $dst $slot]; # nothing has been left on $dst
+    assert_equal OK [R $src slotscheck]
+    assert_equal OK [R $dst slotscheck]
+    puts ">>> Migrate slot_$slot{#$dst => #$src} by SLOTSMGRTTAGSLOT: PASS"
+
+    # verify the data isn't corrupted or changed after 2 migrations
+    assert_equal $dig_src [R $src debug digest]
+    puts -nonewline ">>> End of the case: "
+}
